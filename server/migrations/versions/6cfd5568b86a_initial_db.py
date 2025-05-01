b4182295e3e84fc24ab303f7620689dfda5537d9
Adrian Garza <adriangarza236@yahoo.com>
2025-05-01 13:13:43 -0500

    initial commit

---
+++ server/migrations/versions/6cfd5568b86a_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 6cfd5568b86a
+Revises: 
+Create Date: 2025-05-01 13:13:35.316505
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6cfd5568b86a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
