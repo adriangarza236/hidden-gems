98ab737f888e623eff201d9d0f83d4e70b2086b4
Adrian Garza <adriangarza236@yahoo.com>
2025-06-11 10:42:22 -0500

    hooked up commentSlice to store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../features/auth/authSlice'
 import gemReducer from '../features/auth/gemSlice'
+import commentReducer from '../features/commentSlice'
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
         gems: gemReducer,
+        comments: commentReducer,
     },
 })
 
