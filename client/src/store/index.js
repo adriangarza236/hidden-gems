5b93beae1f526e870a4e94ca8c30bd15cfbc63ad
Adrian Garza <adriangarza236@yahoo.com>
2025-06-12 10:15:02 -0500

    hooked up gemTagSlice to store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../features/auth/authSlice'
 import gemReducer from '../features/gemSlice'
 import commentReducer from '../features/commentSlice'
+import gemTagReducer from '../features/gemTagSlice'
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
         gems: gemReducer,
         comments: commentReducer,
+        gemTags: gemTagReducer,
     },
 })
 
