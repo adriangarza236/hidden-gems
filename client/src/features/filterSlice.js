d0c0e8af30a8552428c0edb73b49c757cc9c0685
Adrian Garza <adriangarza236@yahoo.com>
2025-06-16 13:36:54 -0500

    added filterSlice

---
+++ client/src/features/filterSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    selectedTags: [],
+    searchQuery: "",
+}
+
+const filterSlice = createSlice({
+    name: 'filters',
+    initialState,
+    reducers: {
+        toggleTag(state, action) {
+            const tagId = action.payload
+            if (state.selectedTags.includes(tagId)) {
+                state.selectedTags = state.selectedTags.filter(id => id !== tagId)
+            } else {
+                state.selectedTags.push(tagId)
+            }
+        },
+        clearTags(state) {
+            state.selectedTags = []
+        },
+        setSearchQuery(state, action) {
+            state.searchQuery = action.payload
+        },
+        clearFilters(state) {
+            state.selectedTags = []
+            state.searchQuery = ""
+        }
+    }
+})
+
+export const { toggleTag, clearTags, setSearchQuery, clearFilters } = filterSlice.actions
+export defualt filterSlice.reducer
